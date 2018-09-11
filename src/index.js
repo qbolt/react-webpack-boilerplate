2c561088de1c5448e3210ea5935d7c376c7c5585
Quinton Bolt <qbolt@cooksys.com>
2018-09-11 16:35:47 -0500

    create boilerplate

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './containers/Home'
+import About from './containers/About'
+import Header from './components/Header'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={Header}/>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('app')
+)
